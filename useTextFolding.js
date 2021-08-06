965d835ff6613b255a93753a71bc88417e44ce80
Adam Rybinski <adam.rybins@gmail.com>
2021-08-06 15:47:28 +0200

    Create useTextFolding.js

---
+++ useTextFolding.js
@@ -0,0 +1,36 @@
+import { useState, useEffect, useRef } from "https://unpkg.com/haunted/haunted.js";
+
+export default function useTextFolding(hooks){
+  const useInstance = (instance) => {
+    const { text, collapsed: isCollapsed = true, isFetching } = instance;
+    const [collapsed, setCollapsed] = useState(isCollapsed);
+    const [needToGrow, setNeedToGrow] = useState(false);
+    const textWrapperRef = useRef();
+
+    useEffect(() => {
+      const { current } = descriptionWrapperRef;
+      const handleResize = () => {
+        if (current && (current.clientWidth < current.scrollWidth || current.clientHeight < current.scrollHeight)) {
+          setNeedToGrow(true);
+        } else if (collapsed) {
+          setNeedToGrow(false);
+        }
+      };
+
+      if (current) {
+        window.addEventListener('resize', handleResize);
+        handleResize();
+      }
+      return () => {
+        if (current) {
+          window.removeEventListener('resize', handleResize);
+        }
+      };
+    }, [textWrapperRef, text, isFetching, collapsed]);
+
+    Object.assign(instance, {
+      collapsed, setCollapsed, text, needToGrow, textWrapperRef,
+    });
+  };
+  hooks.useInstance.push(useInstance);
+};
