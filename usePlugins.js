4d0547219d09fdd2cc0aef7f026974ababfe6f0c
Adam Rybinski <adam.rybins@gmail.com>
2021-08-06 15:37:50 +0200

    Create usePlugins.js

---
+++ usePlugins.js
@@ -0,0 +1,31 @@
+import { useRef } from "https://unpkg.com/haunted/haunted.js";
+import { useUpdateInstance } from './useUpdateInstance';
+
+export default function usePlugins(props, ...customPlugins){
+  const instanceRef = useRef({});
+  const useGetLatest = (obj) => {
+    const ref = useRef();
+    ref.current = obj;
+    return React.useCallback(() => ref.current, []);
+  };
+  const getInstance = useGetLatest(instanceRef.current);
+  const defaultPlugins = [];
+  const plugins = [...defaultPlugins, ...customPlugins];
+  Object.assign(getInstance(), {
+    ...props,
+    plugins,
+    hooks: {
+      useInstance: [],
+    },
+  });
+  plugins.forEach((plugin) => {
+    plugin(getInstance().hooks);
+  });
+  const getHooks = useGetLatest(getInstance().hooks);
+  getInstance().getHooks = getHooks;
+  delete getInstance().hooks;
+  getHooks().useInstance.forEach((hook) => {
+    hook(getInstance());
+  });
+  return getInstance();
+};
