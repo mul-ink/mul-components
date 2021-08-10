f6a88c6944def4912508ed7daea13e8079cf5586
Adam Rybinski <arybinski@ibm.com>
2021-08-10 12:34:19 +0200

    Add extensions

--- deps.js
+++ deps.js
@@ -1,5 +1,5 @@
 export { supportsAdoptingStyleSheets } from 'https://cdn.skypack.dev/lit';
 export {  html, component, useState, useEffect, useLayoutEffect } from 'https://unpkg.com/haunted/haunted.js';
-export { useConstructableStylesheets } from './useConstructableStylesheets';
-export { usePlugins } from './usePlugins';
-export { useTextFolding } from './useTextFolding';
+export { useConstructableStylesheets } from './useConstructableStylesheets.js';
+export { usePlugins } from './usePlugins.js';
+export { useTextFolding } from './useTextFolding.js';
