a82db894ca636954bda640ce7df317edf80d5463
Adam Rybinski <adam.rybins@gmail.com>
2021-08-11 13:00:56 +0200

    Export css

--- deps.js
+++ deps.js
@@ -1,4 +1,4 @@
-export { supportsAdoptingStyleSheets } from 'https://cdn.skypack.dev/lit';
+export { supportsAdoptingStyleSheets, css } from 'https://cdn.skypack.dev/lit';
 export {  html, component, useState, useEffect, useLayoutEffect, useRef } from 'https://unpkg.com/haunted/haunted.js';
 export { useConstructableStylesheets } from './useConstructableStylesheets.js';
 export { usePlugins } from './usePlugins.js';
