4a8570d0b1d9f8f3b32eeabd1ae0b85737d2a4c6
Adam Rybinski <adam@rybinski.info>
2021-08-12 19:54:09 +0200

    Update

--- mul-button.js
+++ mul-button.js
@@ -1,18 +1,16 @@
-import {
-  html,
-  component,
-  css,
-  useCSS,
-} from "https://deno.land/x/mulink@0.0.16/deps.js"
-
-function MulButton({ disabled, onclick }) {
-  useCSS(this, [mulButtonCss])
+import { useCSS, html, css, component } from 'https://deno.land/x/mulink@0.0.16/deps.js';
+function MulButton(element) {
+  useCSS(element, [mulButtonCss])
   return html`
+    ${buttonTemplate(element)}
+  `
+}
+
+const buttonTemplate = ({ disabled, onclick}) => html`
     <button type="button" ?disabled=${disabled} @click=${onclick}>
       <slot></slot>
     </button>
-  `
-}
+`
 
 MulButton.observedAttributes = ['disabled', 'onclick']
 
