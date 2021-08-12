7094a793b3dcaaee83bc1463f780af4dd4a1cf53
Adam Rybinski <adam.rybins@gmail.com>
2021-08-12 16:50:08 +0200

    Update deps.js

--- deps.js
+++ deps.js
@@ -1,5 +1,5 @@
 export { supportsAdoptingStyleSheets, css } from 'https://cdn.skypack.dev/lit';
 export {  html, component, useState, useEffect, useLayoutEffect, useRef } from 'https://unpkg.com/haunted/haunted.js';
-export { useConstructableStylesheets } from './useCSS.js';
+export { useCSS } from './useCSS.js';
 export { usePlugins } from './usePlugins.js';
 export { useTextFolding } from './useTextFolding.js';
