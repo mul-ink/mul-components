021596831ad4692f97f3991c59f30427bedaca3f
Adam Rybinski <adam@rybinski.info>
2021-08-14 08:56:45 +0200

    Init get by text scriplet

--- scriplets.js
+++ scriplets.js
@@ -2,3 +2,26 @@
 (function() {
     console.log('Hello scriplet!');    
 })();
+
+/// reddit
+(() => {
+const waitForElementByText = (text) => new Promise(resolve => {
+    
+    if (document.querySelector(selector)) {
+        return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver(mutations => {
+        if (document.querySelector(selector)) {
+            resolve(document.querySelector(selector));
+            observer.disconnect();
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+});
+const continueOnBrowser = document.evaluate("//button[contains(., 'Continue')]", document, null, XPathResult.ANY_TYPE, null ).iterateNext();
+})();
