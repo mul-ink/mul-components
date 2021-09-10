6e50e69ce70c3c747fbbde8946c4430a3033226b
Adam Rybinski <adam@rybinski.info>
2021-09-11 00:39:09 +0200

    remove jest pup

--- deps.js
+++ deps.js
@@ -1,6 +1,6 @@
-export { supportsAdoptingStyleSheets, css } from 'https://cdn.skypack.dev/lit';
-export {  render, html, component, useState, useEffect, useLayoutEffect, useRef, useCallback } from 'https://unpkg.com/haunted/haunted.js';
-export { useCSS } from './useCSS.js';
-export { usePlugins } from './usePlugins.js';
-export { useTextFolding } from './useTextFolding.js';
+export { supportsAdoptingStyleSheets, css } from 'https://cdn.skypack.dev/lit'
+export {  render, html, component, useState, useEffect, useLayoutEffect, useRef, useCallback } from 'https://unpkg.com/haunted/haunted.js'
+export { useCSS } from './useCSS.js'
+export { usePlugins } from './usePlugins.js'
+export { useTextFolding } from './useTextFolding.js'
 export { createMachine, interpret, assign } from 'https://cdn.skypack.dev/xstate'
