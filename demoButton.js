d2cf3bd1677d217371024f3e9efc46e0483850ef
Adam Rybinski <adam@rybinski.info>
2021-09-19 20:05:17 +0200

    Adjust demo

---
+++ demoButton.js
@@ -0,0 +1,20 @@
+import { css, html, render } from './deps.js';
+import { darkBackground } from './cssTokens.js';
+
+const bodyCSS = css`
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${darkBackground};
+    height: 300px;
+  }
+`
+
+const demoStyle = html`
+  <style>
+    ${bodyCSS}
+  </style>
+`
+const head = document.head || document.getElementsByTagName('head')[0];
+render(demoStyle, head);
