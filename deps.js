525f00167f69a45ff145542a6219a7c90d46ca9d
Adam Rybinski <adam.rybins@gmail.com>
2021-11-03 16:25:41 +0100

    Update deps.js

--- deps.js
+++ deps.js
@@ -1,6 +1,2 @@
-export { supportsAdoptingStyleSheets, css } from 'https://cdn.skypack.dev/lit'
-export { useCSS } from './useCSS.js'
-export { usePlugins } from './usePlugins.js'
-export { useTextFolding } from './useTextFolding.js'
 export { createMachine, interpret, assign, State, actions } from 'https://cdn.skypack.dev/xstate'
 export { default as html } from "https://jspm.dev/solid-js/html";
